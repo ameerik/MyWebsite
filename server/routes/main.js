db4f1b09fe97ac1b38da948198ad4c0b776415cf
ameerik <ameerinderkajee7@gmail.com>
2025-04-21 21:51:58 +0200

    Connected to MongoDB, added Blog Model, added post display with pagination

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,16 +1,49 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/Post');
 
 
-// Routes
-router.get('', (req, res) => {
-    const locals = {
-        title: "Nodejs Blog ",
-        description: "Simple blog created with NodeJs, Express & MongoDb."
+
+/*
+ * GET/
+ * HOME
+*/
+router.get('', async (req, res) => {
+    try {
+      const locals = {
+        title: "NodeJs Blog",
+        description: "Simple Blog created with NodeJs, Express & MongoDb."
+      }
+  
+      let perPage = 10;
+      let page = req.query.page || 1;
+  
+      const data = await Post.aggregate([ { $sort: { createdAt: -1 } } ])
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec();
+  
+      // Count is deprecated so I am using countDocuments
+      // Old code: const count = await Post.count();
+      const count = await Post.countDocuments({});
+      const nextPage = parseInt(page) + 1;
+      const hasNextPage = nextPage <= Math.ceil(count / perPage);
+  
+      res.render('index', { 
+        locals,
+        data,
+        current: page,
+        nextPage: hasNextPage ? nextPage : null,
+        currentRoute: '/'
+      });
+  
+    } catch (error) {
+      console.log(error);
     }
-
-    res.render('index', { locals });
+  
 });
+
+
 
 // Routes
 router.get('/about', (req, res) => {
@@ -18,3 +51,94 @@ router.get('/about', (req, res) => {
 });
 
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+router.get('', async (req, res) => {
+    const locals = {
+        title: "Nodejs Blog ",
+        description: "Simple blog created with NodeJs, Express & MongoDb."
+    }
+
+    try {
+        const data = await Post.find();
+        res.render('index', { locals, data });
+    } catch (error) {
+        console.log(error);
+    }
+
+});
+*/
+
+
+
+
+/*
+function insertPostData () {
+    Post.insertMany([
+        {
+            title: "Building a Blog",
+            body: "This is the body text"
+        },
+                {
+                  title: "Building APIs with Node.js",
+                  body: "Learn how to use Node.js to build RESTful APIs using frameworks like Express.js"
+                },
+                {
+                  title: "Deployment of Node.js applications",
+                  body: "Understand the different ways to deploy your Node.js applications, including on-premises, cloud, and container environments..."
+                },
+                {
+                  title: "Authentication and Authorization in Node.js",
+                  body: "Learn how to add authentication and authorization to your Node.js web applications using Passport.js or other authentication libraries."
+                },
+                {
+                  title: "Understand how to work with MongoDB and Mongoose",
+                  body: "Understand how to work with MongoDB and Mongoose, an Object Data Modeling (ODM) library, in Node.js applications."
+                },
+                {
+                  title: "build real-time, event-driven applications in Node.js",
+                  body: "Socket.io: Learn how to use Socket.io to build real-time, event-driven applications in Node.js."
+                },
+                {
+                  title: "Discover how to use Express.js",
+                  body: "Discover how to use Express.js, a popular Node.js web framework, to build web applications."
+                },
+                {
+                  title: "Asynchronous Programming with Node.js",
+                  body: "Asynchronous Programming with Node.js: Explore the asynchronous nature of Node.js and how it allows for non-blocking I/O operations."
+                },
+                {
+                  title: "Learn the basics of Node.js and its architecture",
+                  body: "Learn the basics of Node.js and its architecture, how it works, and why it is popular among developers."
+                },
+                {
+                  title: "NodeJs Limiting Network Traffic",
+                  body: "Learn how to limit netowrk traffic."
+                },
+                {
+                  title: "Learn Morgan - HTTP Request logger for NodeJs",
+                  body: "Learn Morgan."
+                },
+    ])
+}
+insertPostData();
+*/
