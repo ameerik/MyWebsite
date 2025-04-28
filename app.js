47207c6da5adf028324baa6da2ccb682507ca87b
ameerik <ameerinderkajee7@gmail.com>
2025-04-28 15:42:17 +0200

    started working on login page for admin functionality

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
 app.use('/', require('./server/routes/main'));
-app.use('/about' , require('./server/routes/main'));
+app.use('/' , require('./server/routes/admin'));
 
 app.listen(PORT, ()=>{
     console.log(`App listening on port ${PORT}`);
